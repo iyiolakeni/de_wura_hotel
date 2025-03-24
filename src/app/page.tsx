92df563654edc191580466beb2d766a88cacae97
iyiolakeni <iawe@polarisbanklimited.com>
2025-03-24 17:08:42 +0100

    Add new dependencies and enhance responsive styles for room details and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,89 +1,57 @@
-import { FeaturedRoom } from "./model/room_interface";
+"use client";
 import styles from "./page.module.css";
 import style from "./featuredroom.module.css";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { all_rooms } from "./model/room";
 
 export default function Home() {
-	const featured_rooms: FeaturedRoom[] = [
-		{
-			id: 1,
-			name: "Presidential Suit",
-			description:
-				"Experience luxury with breathtaking ocean views,premium amenities, and sophisticated comfort.",
-			price: 40000,
-			image: "/assets/room1.svg",
-		},
-		{
-			id: 2,
-			name: "Executive Room",
-			description:
-				"Experience luxury with breathtaking ocean views,premium amenities, and sophisticated comfort.",
-			price: 30000,
-			image: "/assets/room1.svg",
-		},
-		{
-			id: 3,
-			name: "Standard Room",
-			description:
-				"Experience luxury with breathtaking ocean views,premium amenities, and sophisticated comfort.",
-			price: 25000,
-			image: "/assets/room1.svg",
-		},
-		{
-			id: 4,
-			name: "Kings Size Room 1",
-			description:
-				"Experience luxury with breathtaking ocean views,premium amenities, and sophisticated comfort.",
-			price: 25000,
-			image: "/assets/room1.svg",
-		},
-		{
-			id: 5,
-			name: "Kings Single Room 2",
-			description:
-				"Experience luxury with breathtaking ocean views,premium amenities, and sophisticated comfort.",
-			price: 20000,
-			image: "/assets/room1.svg",
-		},
-	];
+  const router = useRouter();
+  const featured_rooms = all_rooms;
 
-	return (
-		<div className={styles.page}>
-			<div className={styles.main_view}>
-				<div className={styles.content}>
-					<h1>Experience Luxury</h1>
-					<h1>Beyond Compare</h1>
-					<p>
-						Discover an oasis of tranquility and elegance at our five-star
-						resort, where every moment is crafted to perfection.
-					</p>
-					<button>Explore Our Rooms</button>
-				</div>
-			</div>
+  const routeToRoom = (route: string) => {
+    router.push(`/room/${route}`);
+  };
 
-			<div className={style.featured_room}>
-				<h2>Featured Rooms</h2>
-				<div className={style.room_container}>
-					{featured_rooms.map((room) => (
-						<div key={room.id} className={style.room}>
-							<Image
-								src={room.image}
-								alt={room.name}
-								width={500}
-								height={350}
-							/>
-							<div className={style.room_content}>
-								<h3>{room.name}</h3>
-								<p>{room.description}</p>
-								<div className={style.more}>
-									<p>From ₦{room.price}/night</p>
-									<button>View Details</button>
-								</div>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.page}>
+      <div className={styles.main_view}>
+        <div className={styles.content}>
+          <h1>Experience Luxury</h1>
+          <h1>Beyond Compare</h1>
+          <p>
+            Discover an oasis of tranquility and elegance at our five-star
+            resort, where every moment is crafted to perfection.
+          </p>
+          <button>Explore Our Rooms</button>
+        </div>
+      </div>
+
+      <div className={style.featured_room}>
+        <h2>Featured Rooms</h2>
+        <div className={style.room_container}>
+          {featured_rooms.map((room) => (
+            <div key={room.id} className={style.room}>
+              <Image
+                src={room.image1}
+                alt={room.name}
+                width={500}
+                height={350}
+              />
+              <div className={style.room_content}>
+                <h3>{room.name}</h3>
+                <p>{room.description}</p>
+                <div className={style.more}>
+                  <p>From ₦{room.price}/night</p>
+                  <button onClick={() => routeToRoom(room.route)}>
+                    View Details
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -5,273 +5,284 @@ import styles from "./all_room.module.css";
 import { useRouter } from "next/navigation";
 
 export default function Room() {
-	const router = useRouter();
-	const all_rooms: RoomType[] = [
-		{
-			id: 1,
-			name: "Presidential Suit",
-			route: "presidential-suit",
-			price: 40000,
-			image: "/assets/room1.svg",
-			status: "Available",
-			length: 45,
-			no_of_guests: 4,
-			bed: "King Size",
-			extra1: "Free Wifi",
-			extra1Image: "/assets/wifi.svg",
-			extra2: "Smart TV",
-			extra2Image: "/assets/tv.svg",
-			extra3: "AC",
-			extra3Image: "/assets/ac.svg",
-			extra4: "Mini Bar",
-			extra4Image: "/assets/bar.svg",
-		},
-		{
-			id: 2,
-			name: "Executive Room",
-			route: "executive-room",
-			price: 30000,
-			image: "/assets/room1.svg",
-			status: "Available",
-			length: 45,
-			no_of_guests: 4,
-			bed: "King Size",
-			extra1: "Free Wifi",
-			extra1Image: "/assets/wifi.svg",
-			extra2: "Smart TV",
-			extra2Image: "/assets/tv.svg",
-			extra3: "AC",
-			extra3Image: "/assets/ac.svg",
-			extra4: "Mini Bar",
-			extra4Image: "/assets/bar.svg",
-		},
-		{
-			id: 3,
-			name: "Standard Room",
-			route: "standard-room",
-			price: 25000,
-			image: "/assets/room1.svg",
-			status: "Available",
-			length: 45,
-			no_of_guests: 4,
-			bed: "King Size",
-			extra1: "Free Wifi",
-			extra1Image: "/assets/wifi.svg",
-			extra2: "Smart TV",
-			extra2Image: "/assets/tv.svg",
-			extra3: "AC",
-			extra3Image: "/assets/ac.svg",
-			extra4: "Mini Bar",
-			extra4Image: "/assets/bar.svg",
-		},
-		{
-			id: 4,
-			name: "Kings Size Room 1",
-			route: "kings-size-room-1",
-			price: 25000,
-			image: "/assets/room1.svg",
-			status: "Available",
-			length: 45,
-			no_of_guests: 4,
-			bed: "King Size",
-			extra1: "Free Wifi",
-			extra1Image: "/assets/wifi.svg",
-			extra2: "Smart TV",
-			extra2Image: "/assets/tv.svg",
-			extra3: "AC",
-			extra3Image: "/assets/ac.svg",
-			extra4: "Mini Bar",
-			extra4Image: "/assets/bar.svg",
-		},
-		{
-			id: 5,
-			name: "Kings Single Room 2",
-			route: "kings-single-room-2",
-			price: 20000,
-			image: "/assets/room1.svg",
-			status: "Available",
-			length: 45,
-			no_of_guests: 4,
-			bed: "King Size",
-			extra1: "Free Wifi",
-			extra1Image: "/assets/wifi.svg",
-			extra2: "Smart TV",
-			extra2Image: "/assets/tv.svg",
-			extra3: "AC",
-			extra3Image: "/assets/ac.svg",
-			extra4: "Mini Bar",
-			extra4Image: "/assets/bar.svg",
-		},
-	];
+  const router = useRouter();
+  const all_rooms: RoomType[] = [
+    {
+      id: 1,
+      name: "Presidential Suit",
+      route: "presidential-suit",
+      price: 40000,
+      image: "/assets/room1.svg",
+      status: "Available",
+      length: 45,
+      no_of_guests: 4,
+      bed: "King Size",
+      extra1: "Free Wifi",
+      extra1Image: "/assets/wifi.svg",
+      extra2: "Smart TV",
+      extra2Image: "/assets/tv.svg",
+      extra3: "AC",
+      extra3Image: "/assets/ac.svg",
+      extra4: "Mini Bar",
+      extra4Image: "/assets/bar.svg",
+    },
+    {
+      id: 2,
+      name: "Executive Room",
+      route: "executive-room",
+      price: 30000,
+      image: "/assets/room1.svg",
+      status: "Available",
+      length: 45,
+      no_of_guests: 4,
+      bed: "King Size",
+      extra1: "Free Wifi",
+      extra1Image: "/assets/wifi.svg",
+      extra2: "Smart TV",
+      extra2Image: "/assets/tv.svg",
+      extra3: "AC",
+      extra3Image: "/assets/ac.svg",
+      extra4: "Mini Bar",
+      extra4Image: "/assets/bar.svg",
+    },
+    {
+      id: 3,
+      name: "Standard Room",
+      route: "standard-room",
+      price: 25000,
+      image: "/assets/room1.svg",
+      status: "Available",
+      length: 45,
+      no_of_guests: 4,
+      bed: "King Size",
+      extra1: "Free Wifi",
+      extra1Image: "/assets/wifi.svg",
+      extra2: "Smart TV",
+      extra2Image: "/assets/tv.svg",
+      extra3: "AC",
+      extra3Image: "/assets/ac.svg",
+      extra4: "Mini Bar",
+      extra4Image: "/assets/bar.svg",
+    },
+    {
+      id: 4,
+      name: "Kings Size Room 1",
+      route: "kings-size-room-1",
+      price: 25000,
+      image: "/assets/room1.svg",
+      status: "Available",
+      length: 45,
+      no_of_guests: 4,
+      bed: "King Size",
+      extra1: "Free Wifi",
+      extra1Image: "/assets/wifi.svg",
+      extra2: "Smart TV",
+      extra2Image: "/assets/tv.svg",
+      extra3: "AC",
+      extra3Image: "/assets/ac.svg",
+      extra4: "Mini Bar",
+      extra4Image: "/assets/bar.svg",
+    },
+    {
+      id: 5,
+      name: "Kings Single Room 2",
+      route: "kings-single-room-2",
+      price: 20000,
+      image: "/assets/room1.svg",
+      status: "Available",
+      length: 45,
+      no_of_guests: 4,
+      bed: "King Size",
+      extra1: "Free Wifi",
+      extra1Image: "/assets/wifi.svg",
+      extra2: "Smart TV",
+      extra2Image: "/assets/tv.svg",
+      extra3: "AC",
+      extra3Image: "/assets/ac.svg",
+      extra4: "Mini Bar",
+      extra4Image: "/assets/bar.svg",
+    },
+  ];
 
-	const room_comparison: RoomComparison[] = [
-		{
-			id: 1,
-			name: "Presidential Suit",
-			room_size: 32,
-			price: 40000,
-			bed_type: "King Size",
-		},
-		{
-			id: 2,
-			name: "Executive Room",
-			price: 30000,
-			room_size: 45,
-			bed_type: "King Size",
-		},
-		{
-			id: 3,
-			name: "Standard Room",
-			price: 25000,
-			bed_type: "King Size",
-			room_size: 50,
-		},
-		{
-			id: 4,
-			name: "Kings Size Room 1",
-			price: 25000,
-			bed_type: "King Size",
-			room_size: 40,
-		},
-		{
-			id: 5,
-			name: "Kings Single Room 2",
-			price: 20000,
-			room_size: 60,
-			bed_type: "King Size",
-		},
-	];
-	return (
-		<div className={styles.rooms}>
-			<div className={styles.header}>
-				<h2>Our Luxury Accomodation</h2>
-				<p>
-					Experience unparalleled comfort in our thoughtfully designed rooms and
-					suites, each offering a perfect blend of luxury and functionality.
-				</p>
-			</div>
+  const room_comparison: RoomComparison[] = [
+    {
+      id: 1,
+      name: "Presidential Suit",
+      room_size: 32,
+      price: 40000,
+      bed_type: "King Size",
+    },
+    {
+      id: 2,
+      name: "Executive Room",
+      price: 30000,
+      room_size: 45,
+      bed_type: "King Size",
+    },
+    {
+      id: 3,
+      name: "Standard Room",
+      price: 25000,
+      bed_type: "King Size",
+      room_size: 50,
+    },
+    {
+      id: 4,
+      name: "Kings Size Room 1",
+      price: 25000,
+      bed_type: "King Size",
+      room_size: 40,
+    },
+    {
+      id: 5,
+      name: "Kings Single Room 2",
+      price: 20000,
+      room_size: 60,
+      bed_type: "King Size",
+    },
+  ];
+  return (
+    <div className={styles.rooms_container}>
+      <div className={styles.rooms}>
+        <div className={styles.header}>
+          <h2>Our Luxury Accomodation</h2>
+          <p>
+            Experience unparalleled comfort in our thoughtfully designed rooms
+            and suites, each offering a perfect blend of luxury and
+            functionality.
+          </p>
+        </div>
+      </div>
 
-			<div className={styles.room_container}>
-				{all_rooms.map((room) => (
-					<div key={room.id} className={styles.room}>
-						<Image src={room.image} alt={room.name} width={100} height={100} />
-						<div className={styles.room_content}>
-							<div className={styles.room_content_header}>
-								<div className={styles.room_content_head}>
-									<h2>{room.name}</h2>
-									<p>From ₦{room.price} per night</p>
-								</div>
-								<p className={styles.status}>{room.status}</p>
-							</div>
-							<div className={styles.details}>
-								<div className={styles.detail}>
-									<Image
-										alt="Meter"
-										src={"/assets/room_length.svg"}
-										width={20}
-										height={20}
-									/>
-									<p>{room.length} meter</p>
-								</div>
-								<div className={styles.detail}>
-									<Image
-										alt="Meter"
-										src={"/assets/people.svg"}
-										width={20}
-										height={20}
-									/>
-									<p>{room.no_of_guests} Guests</p>
-								</div>
-								<div className={styles.detail}>
-									<Image
-										alt="Meter"
-										src={"/assets/bed.svg"}
-										width={20}
-										height={20}
-									/>
-									<p>{room.bed}</p>
-								</div>
-							</div>
-							<hr />
-							<div className={styles.extra}>
-								<div className={styles.extra_details}>
-									<Image
-										src={room.extra1Image || ""}
-										alt={room.extra1 || ""}
-										width={20}
-										height={20}
-									/>
-									<p>{room.extra1}</p>
-								</div>
-								<div className={styles.extra_details}>
-									<Image
-										src={room.extra2Image || ""}
-										alt={room.extra2 || ""}
-										width={20}
-										height={20}
-									/>
-									<p>{room.extra2}</p>
-								</div>
-								<div className={styles.extra_details}>
-									<Image
-										src={room.extra3Image || ""}
-										alt={room.extra3 || ""}
-										width={20}
-										height={20}
-									/>
-									<p>{room.extra3}</p>
-								</div>
-								<div className={styles.extra_details}>
-									<Image
-										src={room.extra4Image || ""}
-										alt={room.extra4 || ""}
-										width={20}
-										height={20}
-									/>
-									<p>{room.extra4}</p>
-								</div>
-							</div>
-							<button onClick={() => router.push(`/room/${room.route}`)}>
-								View More
-							</button>
-						</div>
-					</div>
-				))}
-			</div>
-
-			<div className={styles.compare}>
-				<h2>Compare Room Types</h2>
-				<table>
-					<thead>
-						<tr>
-							<td>Features</td>
-							{room_comparison.map((room) => (
-								<td key={room.id}>{room.name}</td>
-							))}
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Room Size</td>
-							{room_comparison.map((room) => (
-								<td key={room.id}>{room.room_size} m²</td>
-							))}
-						</tr>
-						<tr>
-							<td>Price</td>
-							{room_comparison.map((room) => (
-								<td key={room.id}>{room.price}</td>
-							))}
-						</tr>
-						<tr>
-							<td>Bed Type</td>
-							{room_comparison.map((room) => (
-								<td key={room.id}>{room.bed_type}</td>
-							))}
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+      <div>
+        <div className={styles.room_container}>
+          {all_rooms.map((room) => (
+            <div key={room.id} className={styles.room}>
+              <Image
+                src={room.image}
+                alt={room.name}
+                width={100}
+                height={100}
+              />
+              <div className={styles.room_content}>
+                <div className={styles.room_content_header}>
+                  <div className={styles.room_content_head}>
+                    <h2>{room.name}</h2>
+                    <p>From ₦{room.price} per night</p>
+                  </div>
+                  <p className={styles.status}>{room.status}</p>
+                </div>
+                <div className={styles.details}>
+                  <div className={styles.detail}>
+                    <Image
+                      alt="Meter"
+                      src={"/assets/room_length.svg"}
+                      width={20}
+                      height={20}
+                    />
+                    <p>{room.length} meter</p>
+                  </div>
+                  <div className={styles.detail}>
+                    <Image
+                      alt="Meter"
+                      src={"/assets/people.svg"}
+                      width={20}
+                      height={20}
+                    />
+                    <p>{room.no_of_guests} Guests</p>
+                  </div>
+                  <div className={styles.detail}>
+                    <Image
+                      alt="Meter"
+                      src={"/assets/bed.svg"}
+                      width={20}
+                      height={20}
+                    />
+                    <p>{room.bed}</p>
+                  </div>
+                </div>
+                <hr />
+                <div className={styles.extra}>
+                  <div className={styles.extra_details}>
+                    <Image
+                      src={room.extra1Image || ""}
+                      alt={room.extra1 || ""}
+                      width={20}
+                      height={20}
+                    />
+                    <p>{room.extra1}</p>
+                  </div>
+                  <div className={styles.extra_details}>
+                    <Image
+                      src={room.extra2Image || ""}
+                      alt={room.extra2 || ""}
+                      width={20}
+                      height={20}
+                    />
+                    <p>{room.extra2}</p>
+                  </div>
+                  <div className={styles.extra_details}>
+                    <Image
+                      src={room.extra3Image || ""}
+                      alt={room.extra3 || ""}
+                      width={20}
+                      height={20}
+                    />
+                    <p>{room.extra3}</p>
+                  </div>
+                  <div className={styles.extra_details}>
+                    <Image
+                      src={room.extra4Image || ""}
+                      alt={room.extra4 || ""}
+                      width={20}
+                      height={20}
+                    />
+                    <p>{room.extra4}</p>
+                  </div>
+                </div>
+                <button onClick={() => router.push(`/room/${room.route}`)}>
+                  View More
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <div className={styles.compare}>
+          <h2>Compare Room Types</h2>
+          <table>
+            <thead>
+              <tr>
+                <td>Features</td>
+                {room_comparison.map((room) => (
+                  <td key={room.id}>{room.name}</td>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Room Size</td>
+                {room_comparison.map((room) => (
+                  <td key={room.id}>{room.room_size} m²</td>
+                ))}
+              </tr>
+              <tr>
+                <td>Price</td>
+                {room_comparison.map((room) => (
+                  <td key={room.id}>{room.price}</td>
+                ))}
+              </tr>
+              <tr>
+                <td>Bed Type</td>
+                {room_comparison.map((room) => (
+                  <td key={room.id}>{room.bed_type}</td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
